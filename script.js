ef570a71c19d7c4306cda00d60f7431b560f7f8d
jiratchaya0248-hub <jiratchaya0248@gmail.com>
2026-08-18 11:44:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded',function(){
-document.getElementById('year').innerHTML=new Date().getFullYear
+document.getElementById('year').innerHTML=new Date().getFullYear();
   console.log('Portolio ของ JIRATCHAYA พร้อมแล้ว!');
 });
